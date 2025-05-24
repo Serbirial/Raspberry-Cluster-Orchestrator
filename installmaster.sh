992dc60c83aec5366d709ccf6af80428ef1fda99
Serbirial <39264658+Serbirial@users.noreply.github.com>
2025-05-24 00:58:57 -0400

    Forgot to remove clone

--- installmaster.sh
+++ installmaster.sh
@@ -1,6 +1,3 @@
-git clone https://github.com/Serbirial/Raspberry-Cluster-Orchestrator.git
-cd Raspberry-Cluster-Orchestrator
-
 cd master && go build && cp ./master ../master && cd ../
 cd watchdog && go build && cp ./watchdog ../watchdog && cd ../
 echo "Installed! Please configure the watchdog binary to run on start (service file), use the 'master' binary to send commands to workers"
--- installworker.sh
+++ installworker.sh
@@ -1,5 +1,3 @@
-git clone https://github.com/Serbirial/Raspberry-Cluster-Orchestrator.git
-cd Raspberry-Cluster-Orchestrator
 
 cd slave && go build && cp ./slave ../slave && cd ../
 echo "Installed! Please configure the slave binary to run on startup through a service file"
