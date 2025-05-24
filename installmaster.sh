adf233167dbf18ecbdc4c0e9fd11092db146e69e
Serbirial <39264658+Serbirial@users.noreply.github.com>
2025-05-24 01:02:05 -0400

    Fix naming

--- installmaster.sh
+++ installmaster.sh
@@ -1,5 +1,5 @@
-cd master && go build && cp ./master ../master && cd ../
-cd watchdog && go build && cp ./watchdog ../watchdog && cd ../
+cd master && go build && cp ./master ../Master && cd ../
+cd watchdog && go build && cp ./watchdog ../Watchdog && cd ../
 echo "Installed! Please configure the watchdog binary to run on start (service file), use the 'master' binary to send commands to workers"
 
 echo "Running help command for master..."
--- installworker.sh
+++ installworker.sh
@@ -1,3 +1,3 @@
 
-cd slave && go build && cp ./slave ../slave && cd ../
+cd slave && go build && cp ./slave ../Slave && cd ../
 echo "Installed! Please configure the slave binary to run on startup through a service file"
