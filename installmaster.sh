420e23699285f1d42badb331007cc131f498d4e4
Serbirial <39264658+Serbirial@users.noreply.github.com>
2025-05-24 20:33:01 -0400

    Fix

--- installmaster.sh
+++ installmaster.sh
@@ -3,4 +3,4 @@ cd watchdog && go build && cp ./watchdog ../Watchdog && cd ../
 echo "Installed! Please configure the watchdog binary to run on start (service file), use the 'master' binary to send commands to workers"
 
 echo "Running help command for master..."
-./master -help
+./Master -help
